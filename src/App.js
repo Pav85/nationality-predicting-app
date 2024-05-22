2484acae9b2da141784eeeebe95adc428f32a83f
Pawel <pawel.werbowy@gmail.com>
2024-05-22 13:29:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 function App() {
+  const [name, setName] = useState("");
+  const [nationality, setNationality] = useState(null);
+  const inputRef = useRef(null);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
